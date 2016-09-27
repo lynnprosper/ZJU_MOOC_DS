bf55d83cea330e4e9c8ce7173d8cdf7a3f30e628
Miles <liang19931@163.com>
2016-09-27 10:56:03 +0800

    Create 04-树4 是否同一棵二叉搜索树.cpp
    给定一个插入序列就可以唯一确定一棵二叉搜索树。然而，一棵给定的二叉搜索树却可以由多种不同的插入序列得到。例如分别按照序列{2, 1, 3}和{2, 3, 1}插入初始为空的二叉搜索树，都得到一样的结果。于是对于输入的各种插入序列，你需要判断它们是否能生成一样的二叉搜索树。
    
    输入格式:
    
    输入包含若干组测试数据。每组数据的第1行给出两个正整数NN (\le 10≤10)和LL，分别是每个序列插入元素的个数和需要检查的序列个数。第2行给出NN个以空格分隔的正整数，作为初始插入序列。最后LL行，每行给出NN个插入的元素，属于LL个需要检查的序列。
    
    简单起见，我们保证每个插入序列都是1到NN的一个排列。当读到NN为0时，标志输入结束，这组数据不要处理。
    
    输出格式:
    
    对每一组需要检查的序列，如果其生成的二叉搜索树跟对应的初始序列生成的一样，输出“Yes”，否则输出“No”。
    
    输入样例:
    
    4 2
    3 1 4 2
    3 4 1 2
    3 2 4 1
    2 1
    2 1
    1 2
    0
    输出样例:
    
    Yes
    No
    No

---
+++ 04-树4 是否同一棵二叉搜索树.cpp
@@ -0,0 +1,124 @@
+#include <iostream>
+using namespace std;
+typedef struct Node
+{
+	int data, flag;
+	struct Node * left;
+	struct Node *right;
+}node;
+
+node *nwNode(int d);
+void nodeInsert(node *&head, int d);
+node *tBuild(int n);
+int tJudge(node *head, int n);
+void flagReset(node *head);
+void tDelete(node *head);
+
+int main()
+{
+	int n, line, i;
+	cin >> n;
+	while (n)
+	{
+		cin >> line;
+		node *head = tBuild(n);
+		for (i = 0; i < line; i++)
+		{
+			if (tJudge(head, n))
+				cout << "Yes" << endl;
+			else
+				cout << "No" << endl;
+
+			flagReset(head);
+		}
+		tDelete(head);
+
+		cin >> n;
+	}
+
+	return 0;
+}
+
+node *nwNode(int d)
+{
+	node *p = new node;
+	p->data = d;
+	p->flag = 0;
+	p->left = p->right = NULL;
+	return p;
+}
+void nodeInsert(node *&head, int d)
+{
+
+	if (!head)
+		head = nwNode(d);
+	else
+	{
+		if (d > head->data)
+			nodeInsert(head->right, d);
+		else
+			nodeInsert(head->left, d);
+	}
+}
+node *tBuild(int n)
+{
+	int i, d;
+	cin >> d;
+	node *head = nwNode(d);
+	for (i = 1; i < n; i++)
+	{
+		cin >> d;
+		nodeInsert(head, d);
+	}
+	return head;
+}
+int tJudge(node *head, int n)
+{
+	node *T;
+	int i, t, fflag = 1;
+	for (i = 0; i < n; i++)
+	{
+		T = head;
+		cin >> t;
+		while (T && fflag)
+		{
+			if (T->flag == 1)
+			{
+				if (t > T->data)
+					T = T->right;
+				else
+					T = T->left;
+			}
+			else if (T->flag == 0 && T->data == t)
+			{
+				T->flag = 1;
+				break;
+			}
+			else
+			{
+				fflag = 0;
+				break;
+			}
+		}
+	}
+	if (fflag == 0)
+		return 0;
+	else
+		return 1;
+}
+void flagReset(node *head)
+{
+	if (head->left)
+		head->left->flag = 0;
+	if (head->right)
+		head->right->flag = 0;
+	head->flag = 0;
+}
+void tDelete(node *head)
+{
+	if (head->left)
+		tDelete(head->left);
+	if (head->right)
+		tDelete(head->right);
+	delete head;
+}
