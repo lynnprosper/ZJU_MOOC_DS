bfb3cc4b40ce91c95d445c5603f8787c2ddc44d9
Miles <liang19931@163.com>
2016-10-11 16:23:10 +0800

    https://pta.patest.cn/pta/test/1342/exam/4/question/22502
    六度空间”理论又称作“六度分隔（Six Degrees of Separation）”理论。这个理论可以通俗地阐述为：“你和任何一个陌生人之间所间隔的人不会超过六个，也就是说，最多通过五个人你就能够认识任何一个陌生人。”如图1所示。
    
    
    图1 六度空间示意图
    “六度空间”理论虽然得到广泛的认同，并且正在得到越来越多的应用。但是数十年来，试图验证这个理论始终是许多社会学家努力追求的目标。然而由于历史的原因，这样的研究具有太大的局限性和困难。随着当代人的联络主要依赖于电话、短信、微信以及因特网上即时通信等工具，能够体现社交网络关系的一手数据已经逐渐使得“六度空间”理论的验证成为可能。
    
    假如给你一个社交网络图，请你对每个节点计算符合“六度空间”理论的结点占结点总数的百分比。
    
    输入格式:
    
    输入第1行给出两个正整数，分别表示社交网络图的结点数NN（1<N\le 10^41<N≤10
    ​4
    ​​ ，表示人数）、边数MM（\le 33\times N≤33×N，表示社交关系数）。随后的MM行对应MM条边，每行给出一对正整数，分别是该条边直接连通的两个结点的编号（节点从1到NN编号）。
    
    输出格式:
    
    对每个结点输出与该结点距离不超过6的结点数占结点总数的百分比，精确到小数点后2位。每个结节点输出一行，格式为“结点编号:（空格）百分比%”。
    
    输入样例:
    
    10 9
    1 2
    2 3
    3 4
    4 5
    5 6
    6 7
    7 8
    8 9
    9 10
    输出样例:
    
    1: 70.00%
    2: 80.00%
    3: 90.00%
    4: 100.00%
    5: 100.00%
    6: 100.00%
    7: 100.00%
    8: 90.00%
    9: 80.00%
    10: 70.00%

---
+++ 06-图3 六度空间.cpp
@@ -0,0 +1,131 @@
+#include <iostream>
+#include <queue>	//用做队列容器；
+#define MaxN 10001
+using namespace std;
+
+struct Enode	//定义边；
+{
+	int v1, v2;
+};
+struct Avnode	//定义邻接表的邻接点；
+{
+	int data;
+	Avnode * next;
+};
+struct Ahead	//定义邻接表的头结点；也可以不定义结构，这里是为了方便封装；
+{
+	Avnode *phead = nullptr;
+};
+struct Graph	//定义图的结构；
+{
+	int nv;
+	int ne;
+	Ahead ah[MaxN];
+};
+int visited[MaxN] = { 0 };	//存储结点是否访问；
+
+Graph *Init_Graph(int vnum);	//初始化图；
+void Insert_E(Graph *G, Enode *E);	//把边的关系插入图中；
+Graph *Build_G();	//建立图；
+void OutputE(Graph *G);	//输出边；
+int BFS(Graph *G, int v);	//广度遍历；
+
+int main()
+{
+	Graph * G = Build_G();	//建立图；
+	OutputE(G);	//输出边；
+
+	return 0;
+}
+
+Graph *Init_Graph(int vnum)	//初始化的图只有顶点，没有边；
+{
+	Graph *G = new Graph;
+	G->nv = vnum;
+	G->ne = 0;
+
+	return G;
+}
+
+void Insert_E(Graph *G, Enode *E)
+{
+	Avnode *nAv = new Avnode;
+	nAv->data = E->v2;
+	nAv->next = G->ah[E->v1].phead;	//注意指针；
+	G->ah[E->v1].phead = nAv;
+
+	Avnode *nAv2 = new Avnode;	//无向图两边都要插入；
+	nAv2->data = E->v1;
+	nAv2->next = G->ah[E->v2].phead;
+	G->ah[E->v2].phead = nAv2;
+
+	return;
+}
+
+Graph *Build_G()	//建立图；
+{
+	int tnv;
+	Enode *E = new Enode;
+	cin >> tnv;
+	Graph *G = Init_Graph(tnv);
+	cin >> G->ne;
+	for (int i = 0; i < G->ne; i++)
+	{
+		cin >> E->v1 >> E->v2;
+		Insert_E(G, E);
+	}
+
+	return G;
+}
+
+void OutputE(Graph *G)
+{
+	int cnt;
+	for (int i = 1; i <= G->nv; i++)
+	{
+		cnt = BFS(G, i);
+
+		for (int j = 1; j <= G->nv; j++)	//这里需要初始化；
+			visited[j] = 0;
+
+		printf("%d: %.2f%%\n", i, (float)cnt * 100 / G->nv);
+	}
+
+	return;
+}
+
+int BFS(Graph *G, int v)	//广度遍历；
+{
+	visited[v] = 1;
+	int cnt = 1;
+	queue<int> Q;
+	int last = v;
+	Q.push(v);
+	
+	int tail, level = 0;	//tail是每一层最后一个压入的点；
+	Avnode *w;
+	while (!Q.empty())
+	{
+		v = Q.front();
+		Q.pop();
+		for (w = G->ah[v].phead; w; w = w->next)
+		{
+			if (!visited[w->data])
+			{
+				visited[w->data] = 1;
+				cnt++;
+				tail = w->data;
+				Q.push(w->data);
+			}
+		}
+		if (v == last)	//当弹出的结点和最后压入的点相同时，代表该层遍历完毕，切入下一层；
+		{
+			level++;
+			last = tail;
+		}
+		if (level == 6)
+			break;
+	}
+
+	return cnt;
+}
